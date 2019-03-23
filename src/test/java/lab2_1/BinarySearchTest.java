ee684c39a663ef678a819c7197de7cd1de834908
Zawiasa.Jakub <zawiasa.jakub@gmail.com>
2019-03-23 11:51:05 +0100

    testy_v1 testy dla sekwencji dłuższych niż 1 i znalezionego elementu

--- src/test/java/lab2_1/BinarySearchTest.java
+++ src/test/java/lab2_1/BinarySearchTest.java
@@ -10,11 +10,44 @@ import edu.iis.mto.bsearch.SearchResult;
 public class BinarySearchTest {
 
     private final int[] oneElementTab = {1};
+    private final int[] moreThanOneElementTab = {1, 2, 3, 5, 7, 11, 13, 17};
 
     @Test
     public void shouldSayThatElementIsFoundAndIsInPositionOfIndexZeroIfTabLengthIsOne() {
         int key = 1;
         SearchResult searchResult = BinarySearch.search(key, oneElementTab);
+        assertEquals(true, searchResult.isFound());
+        assertEquals(key, oneElementTab[searchResult.getPosition()]);
+    }
+
+    @Test
+    public void shouldSayThatElementIsNotFoundAndIsInPositionOfIndexMinusOneIfTabLengthIsOne() {
+        int key = 0;
+        SearchResult searchResult = BinarySearch.search(key, oneElementTab);
+        assertEquals(false, searchResult.isFound());
+        assertEquals(-1, searchResult.getPosition());
+    }
+
+    @Test
+    public void shouldSayThatFirstElementIsFoundIfTabLengthIsBiggerThanOne() {
+        int key = 1;
+        SearchResult searchResult = BinarySearch.search(key, moreThanOneElementTab);
+        assertEquals(true, searchResult.isFound());
+        assertEquals(key, oneElementTab[searchResult.getPosition()]);
+    }
+
+    @Test
+    public void shouldSayThatMiddleElementIsFoundIfTabLengthIsBiggerThanOne() {
+        int key = 5;
+        SearchResult searchResult = BinarySearch.search(key, moreThanOneElementTab);
+        assertEquals(true, searchResult.isFound());
+        assertEquals(key, oneElementTab[searchResult.getPosition()]);
+    }
+
+    @Test
+    public void shouldSayThatLastElementIsFoundIfTabLengthIsBiggerThanOne() {
+        int key = moreThanOneElementTab[moreThanOneElementTab.length - 1];
+        SearchResult searchResult = BinarySearch.search(key, moreThanOneElementTab);
         assertEquals(true, searchResult.isFound());
         assertEquals(key, oneElementTab[searchResult.getPosition()]);
     }
